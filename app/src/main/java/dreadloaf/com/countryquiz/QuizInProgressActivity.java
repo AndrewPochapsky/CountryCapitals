694fe53a355826cf857faaba933a2744ff75db9e
AndrewPochapsky <andrew.pochapsky@gmail.com>
2018-06-12 22:20:36 -0400

    Added a progress bar timer to quiz

--- app/src/main/java/dreadloaf/com/countryquiz/QuizInProgressActivity.java
+++ app/src/main/java/dreadloaf/com/countryquiz/QuizInProgressActivity.java
@@ -1,14 +1,21 @@
 package dreadloaf.com.countryquiz;
 
+import android.animation.Animator;
+import android.animation.ObjectAnimator;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.LinearInterpolator;
 import android.widget.Button;
 import android.widget.GridLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import java.util.HashMap;
@@ -22,6 +29,9 @@ public class QuizInProgressActivity extends AppCompatActivity implements View.On
     Queue<Question> mQuestions;
     TextView mQuestionTextView;
     GridLayout mButtonGrid;
+    ProgressBar mTimer;
+
+    final int mMaxTimerValue = 100;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -30,6 +40,12 @@ public class QuizInProgressActivity extends AppCompatActivity implements View.On
 
         mQuestionTextView = findViewById(R.id.quiz_inprogress_header);
         mButtonGrid = findViewById(R.id.quiz_button_grid);
+        mTimer = findViewById(R.id.timer);
+
+        // Get the Drawable custom_progressbar
+        Drawable draw=getResources().getDrawable(R.drawable.custom_progressbar);
+        // set the drawable as progress drawable
+        mTimer.setProgressDrawable(draw);
 
         //Get the region chosen by user
         Intent previousActivityIntent = getIntent();
@@ -42,12 +58,44 @@ public class QuizInProgressActivity extends AppCompatActivity implements View.On
             Button button = (Button)mButtonGrid.getChildAt(i);
             button.setOnClickListener(this);
         }
+
+        //Is multiplied by 100 to ensure for a smooth animation, going up by 1 makes it look choppy
+        mTimer.setMax(mMaxTimerValue*100);
+
+        final ObjectAnimator animation = ObjectAnimator.ofInt(mTimer, "progress", 0, mMaxTimerValue * 100);
+
+        animation.setDuration(10000);
+        animation.setInterpolator(new LinearInterpolator());
+        animation.addListener(new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animator) { }
+
+            @Override
+            public void onAnimationEnd(Animator animator) {
+                //if this happens, then the user has not clicked any option in time
+                //start next question and mark this one is wrong
+
+                //TODO: don't just start anim again
+                animation.start();
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animator) { }
+
+            @Override
+            public void onAnimationRepeat(Animator animator) { }
+        });
+        animation.start();
+
+
+
     }
 
     @Override
     public void onClick(View view) {
         Button button = (Button)view;
         Log.d("BUTTON", "Clicked " + button.getText().toString());
+
     }
 
     private Queue<Question> setupQuestions(){
